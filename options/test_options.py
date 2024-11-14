a9c6e6d0d077b99442cf8d21587d06096a05d54e
Rémi <remi.grz@free.fr>
2024-11-14 14:52:17 +0100

    runned test without prerpocess

--- options/test_options.py
+++ options/test_options.py
@@ -1,5 +1,5 @@
 from .base_options import BaseOptions
-
+import numpy as np
 
 class TestOptions(BaseOptions):
     """This class includes test options.
@@ -12,7 +12,7 @@ class TestOptions(BaseOptions):
         parser.add_argument('--phase', type=str, default='test', help='train, val, test, etc')
         #  Dropout and Batchnorm has different behavioir during training and test.
         parser.add_argument('--eval', action='store_true', help='use eval mode during test time.')
-        parser.add_argument('--num_test', type=int, default=830, help='how many test images to run')
+        parser.add_argument('--num_test', type=int, default=np.inf, help='how many test images to run')
         # rewrite devalue values
         parser.set_defaults(model='test')
         # To avoid cropping, the loadSize should be the same as fineSize
